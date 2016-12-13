8b9e2c889af58e203ef18aa0162203e6dd49947b
Brian <riceb53@gmail.com>
2016-12-12 19:54:43 -0500

    Move logic to model using PORO

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,18 +1,6 @@
 class EmployeesController < ApplicationController
   def index
-    employees_hash_array = Unirest.get("http://localhost:3000/api/v1/employees").body
-    @employees =[]
-    employees_hash_array.each do |employee_hash|
-      @employees << Employee.new(
-        first_name: employee_hash['first_name'],
-        last_name: employee_hash['last_name'],
-        dob: employee_hash['dob'],
-        ssn: employee_hash['ssn'],
-        company_email: employee_hash['company_email'],
-        full_name: employee_hash['full_name'],
-        id: employee_hash['id']
-      )
-    end
+    @employees = Employee.all
     render 'index.html.erb'
   end
 
@@ -26,30 +14,30 @@ class EmployeesController < ApplicationController
   end
 
   def create
-    @employee = Unirest.post("http://localhost:3000/api/v1/employees",
-                  headers:{ "Accept" => "application/json" },
-                  parameters:{ :first_name => params[:form_first_name], :last_name => params[:form_last_name] }
-                ).body
-    redirect_to "/employees/#{@employee["id"]}"
+    @employee = Employee.create(
+      :first_name => params[:form_first_name],
+      :last_name => params[:form_last_name]
+    )
+    redirect_to "/employees/#{@employee.id}"
   end
 
   def edit
-    @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}").body
+    @employee = Employee.find(params[:id])
     render 'edit.html.erb'
   end
 
   def update
-    @employee = Unirest.patch("http://localhost:3000/api/v1/employees/#{params[:id]}",
-      headers:{ "Accept" => "application/json" },
-      parameters:{ :first_name => params[:form_first_name], :last_name => params[:form_last_name] }
-    ).body
-    redirect_to "/employees/#{@employee['id']}"
+    @employee = Employee.find(params[:id])
+    @employee.update(
+      first_name: params[:form_first_name],
+      last_name: params[:form_last_name]
+      )
+    redirect_to "/employees/#{@employee.id}"
   end
 
   def destroy
-    Unirest.delete("http://localhost:3000/api/v1/employees/#{params['id']}")
+    employee = Employee.find(params[:id])
+    employee.destroy
     redirect_to "/employees"
   end
 end
-
-
